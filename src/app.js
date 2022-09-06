7a58d67f96f2ed0963f94d2db527667f34087014
Oscar <oscar@ayoy.se>
2022-09-06 07:44:57 +0200

    feat: add initial dummy server

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const Koa = require("koa");
+const app = new Koa();
+
+// Just return a canned response for now.
+app.use(async ctx => {
+    ctx.body = 'Hello, world!';
+});
+
+// Optionally start listening if this is the main module.
+if (typeof module !== "undefined" && require.main === module) {
+    app.listen(3000);
+    console.log('Listening on port 3000.');
+}
