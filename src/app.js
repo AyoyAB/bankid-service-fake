4315f53fe8c26f289b7a6f5215edd20c998cc673
Oscar <oscar@ayoy.se>
2022-09-07 15:38:43 +0200

    chore: add linting and formatting

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const Koa = require("koa");
+const Koa = require('koa');
+
 const app = new Koa();
 
 // Just return a canned response for now.
-app.use(async ctx => {
-    ctx.body = 'Hello, world!';
+app.use(async (ctx) => {
+  ctx.body = 'Hello, world!';
 });
 
 // Optionally start listening if this is the main module.
-if (typeof module !== "undefined" && require.main === module) {
-    app.listen(3000);
-    console.log('Listening on port 3000.');
+if (typeof module !== 'undefined' && require.main === module) {
+  app.listen(3000);
+  console.log('Listening on port 3000.');
 }
