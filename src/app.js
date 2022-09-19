a708200a81c066b68811383d40f9a76a756b4500
Oscar <oscar@ayoy.se>
2022-09-19 07:11:05 +0200

    feat: enable mutual TLS authentication

--- src/app.js
+++ src/app.js
@@ -8,5 +8,16 @@ const app = new Koa();
 
 app.use(router.routes(), router.allowedMethods());
 
-app.listen(3000);
+const httpsOptions = {
+  // Use the supplied or generated server key pair.
+  key: await fs.readFile('data/server.key', { encoding: 'utf8' }),
+  cert: await fs.readFile('data/server.crt', { encoding: 'utf8' }),
+  // Require client certificates.
+  requestCert: true,
+  // Trust the supplied or generated client CA certificate.
+  ca: [await fs.readFile('data/client-ca.crt', { encoding: 'utf8' })],
+};
+
+https.createServer(httpsOptions, app.callback()).listen(3000);
+
 console.log('Listening on port 3000.');
