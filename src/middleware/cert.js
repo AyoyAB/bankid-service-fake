67fb41c3942ebd627360a7f1740299b3210bb6fc
Oscar <oscar@ayoy.se>
2022-11-28 12:52:53 +0100

    fix: correctly parse the tls client certificate

--- src/middleware/cert.js
+++ src/middleware/cert.js
@@ -1,8 +1,10 @@
+import { X509Certificate } from 'node:crypto';
+
 async function certMiddleware(ctx, next) {
   const clientCert = ctx.req.socket.getPeerCertificate(false);
 
   if (clientCert != null && clientCert != {}) {
-    ctx.state.clientCert = clientCert;
+    ctx.state.clientCert = new X509Certificate(clientCert.raw);
   }
 
   await next();
