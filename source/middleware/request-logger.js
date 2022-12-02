282a57c7b911f46974be04730bdcb33d409c6f9e
Oscar Jacobsson <oscar@ayoy.se>
2022-12-02 14:30:54 +0100

    feat: add request logging middleware

---
+++ source/middleware/request-logger.js
@@ -0,0 +1,19 @@
+import log from 'loglevel';
+
+/**
+ * Koa middleware used to log the request body.
+ *
+ * NB: Needs to be placed after the `koa-body` middleware.
+ *
+ * @param {Object} ctx - The request context.
+ * @param {function} next - The next handler in the chain.
+ */
+async function requestLoggerMiddleware(ctx, next) {
+  if (ctx.request.body) {
+    log.debug(`Request body: ${JSON.stringify(ctx.request.body)}.`);
+  }
+
+  await next();
+}
+
+export default requestLoggerMiddleware;
--- source/routers/rpv51.js
+++ source/routers/rpv51.js
@@ -6,12 +6,15 @@ import createAuthHandler from '../handlers/rpv51/auth.js';
 import createSignHandler from '../handlers/rpv51/sign.js';
 import collectHandler from '../handlers/rpv51/collect.js';
 import cancelHandler from '../handlers/rpv51/cancel.js';
+import requestLoggerMiddleware from '../middleware/request-logger.js';
 
 const dispatcher = createDispatcher();
 
 const rpv51 = new Router();
 
 rpv51.use(koaBody());
+rpv51.use(requestLoggerMiddleware);
+
 rpv51.post('/auth', createAuthHandler(dispatcher));
 rpv51.post('/sign', createSignHandler(dispatcher));
 rpv51.post('/collect', collectHandler);
