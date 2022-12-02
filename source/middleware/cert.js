00d5ad618896e6a5f200cd009e7992b216de970e
Oscar Jacobsson <oscar@ayoy.se>
2022-12-02 14:20:11 +0100

    feat: add relying party certificate logging middleware

--- source/middleware/cert.js
+++ source/middleware/cert.js
@@ -1,10 +1,23 @@
 import { X509Certificate } from 'node:crypto';
+import log from 'loglevel';
 
+/**
+ * Koa middleware used to store the TLS client certificate in the request
+ * context.
+ *
+ * @param {Object} ctx - The request context.
+ * @param {function} next - The next handler in the chain.
+ */
 async function certMiddleware(ctx, next) {
   const clientCert = ctx.req.socket.getPeerCertificate(false);
 
   if (clientCert != null && clientCert != {}) {
     ctx.state.clientCert = new X509Certificate(clientCert.raw);
+    log.debug(
+      `Relying Party certificate: ${JSON.stringify(
+        ctx.state.clientCert.toString()
+      )}.`
+    );
   }
 
   await next();
